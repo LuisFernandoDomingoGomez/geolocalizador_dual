d20ba562869cb509c6045fb7f7435e3780b9b297
Luis Fernando Domingo Gómez <al222010066@gmail.com>
2024-04-10 03:00:37 -0600

    Diseño pag principal

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,6 +3,32 @@
 @section('content')
     @include('layouts.headers.cards')
     <div class="container-fluid mt--7">
+        <!-- Carrusel -->
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="dist/images/font_cecytem.jpeg" alt="First slide" style="max-height: 380px;">
+                    <div class="carousel-caption d-none d-md-block">
+                        <button class="btn btn-primary btn-download">Descargar</button>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="dist/images/font.jpg" alt="Second slide" style="max-height: 380px;">
+                    <div class="carousel-caption d-none d-md-block">
+                        <button class="btn btn-primary btn-download">Descargar</button>
+                    </div>
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+        <!-- Fin del carrusel -->
 
         @include('layouts.footers.auth')
     </div>
@@ -11,4 +37,7 @@
 @push('js')
     <script src="{{ asset('argon') }}/vendor/chart.js/dist/Chart.min.js"></script>
     <script src="{{ asset('argon') }}/vendor/chart.js/dist/Chart.extension.js"></script>
+    <!-- Agregar el script de Bootstrap para el carrusel -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 @endpush
